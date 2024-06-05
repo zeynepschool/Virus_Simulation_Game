e425b783f96fd3f050e3c50018a45552d589f3f9
zeynepschool <163359908+zeynepschool@users.noreply.github.com>
2024-06-05 13:09:12 +1200

    Update game_base_v1.py

--- game_base_v1.py
+++ game_base_v1.py
@@ -10,27 +10,3 @@ countries_infected = [
     {"Country": "France", "Infected People": 150000},
     {"Country": "China", "Infected People": 250000},
 ]
-
-#Define the game  
-print("Welcome to Virus Simulation Game\nEach day user can choose to help only one country between 6 countries, within 5 days.")
-print("The number of infected for the helped country decreases by 40%, whereas the other countries increase by 20%.") 
-
-#Print the list to verify
-##for entry in countries_infected:
-##    print(f"Country: {entry['Country']}, Infected People: {entry['Infected People']}")
-
-##print("Initial status of countries:")
-##for entry in countries_infected:
-##    print(f"Country: {entry['Country']}, Infected People: {entry['Infected People']}")
-
-
-# Loop for 5 days
-for day in range(1, 6):
-    print(f"\nDay {day}")
-    print("Countries status:")
-    for entry in countries_infected:
-        print(f"Country: {entry['Country']}, Infected People: {entry['Infected People']}")
-
-
-    #Select Country
-    help_country = input("Enter the country to help: ")
